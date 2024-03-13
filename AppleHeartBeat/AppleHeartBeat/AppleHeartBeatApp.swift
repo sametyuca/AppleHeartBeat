728b9ba0d8829e66d099c789088c670df5aa0c9f
samet yuca <48648879+sametyuca@users.noreply.github.com>
2024-03-13 16:18:59 +0300

    Add files via upload

---
+++ AppleHeartBeat/AppleHeartBeat/AppleHeartBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleHeartBeatApp.swift
+//  AppleHeartBeat
+//
+//  Created by Samet Yuca on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleHeartBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(isOverview: true)
+        }
+    }
+}
